83b16768a187654251ee3876a86355d1631e66c3
Santiago Medina <devsanmedina@protonmail.com>
2022-06-21 20:58:36 -0500

    Add async fastapi

---
+++ async_fastapi/app.py
@@ -0,0 +1,18 @@
+import asyncio
+
+from fastapi import FastAPI
+import httpx
+
+app = FastAPI()
+
+
+async def get_resource() -> dict:
+    async with httpx.AsyncClient() as client:
+        r = await client.get("http://localhost:8000/blocking-resource", timeout=10.0)
+        return r.json()
+
+
+@app.get("/resource")
+async def resource():
+    return await get_resource()
+
---
+++ async_fastapi/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+uvicorn --port 8002 app:app --reload
+

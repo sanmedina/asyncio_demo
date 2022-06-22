899b24acce725d13b13d80efa5975c16d688458f
Santiago Medina <devsanmedina@protonmail.com>
2022-06-21 20:08:47 -0500

    Add synchronours flask

---
+++ sync_flask/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import httpx
+
+app = Flask(__name__)
+
+
+def get_resource() -> dict:
+    r = httpx.get("http://localhost:8000/blocking-resource", timeout=7.0)
+    return r.json()
+
+
+@app.get("/resource")
+def resource():
+    return get_resource()
+
---
+++ sync_flask/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [[ -n $2 ]]; then
+  echo "set"
+  num_workers=$2
+else
+  echo "not set"
+  num_workers=4
+fi
+
+echo "num workers:$num_workers"
+gunicorn -b 'localhost:8001' -w $num_workers 'app:app'
+
